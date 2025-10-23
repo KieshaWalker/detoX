f6665394e4ee8b1f69765c11fff869c38e50cb56
kieshawalker <llcwalkerk@gmail.com>
2025-10-23 13:19:40 -0500

    Fix database URL logic to explicitly check for Heroku Postgres URL

--- detoX/settings.py
+++ detoX/settings.py
@@ -96,16 +96,26 @@ DATABASE_URL = os.getenv('DATABASE_URL')
 ON_HEROKU = os.getenv('DYNO') or os.getenv('HEROKU_SLUG_COMMIT')
 
 if 'ON_HEROKU' in os.environ:
-    # Use Heroku Postgres URL instead of DATABASE_URL which may be overridden
-    heroku_db_url = os.getenv('HEROKU_POSTGRESQL_ONYX_URL') or DATABASE_URL
-    DATABASES = {
-        "default": dj_database_url.config(
-            default=heroku_db_url,
-            conn_max_age=600,
-            conn_health_checks=True,
-            ssl_require=True,
-        ),
-    }
+    # On Heroku, use the Heroku Postgres URL if available, otherwise fall back to DATABASE_URL
+    heroku_postgres_url = os.getenv('HEROKU_POSTGRESQL_ONYX_URL')
+    if heroku_postgres_url:
+        DATABASES = {
+            "default": dj_database_url.config(
+                default=heroku_postgres_url,
+                conn_max_age=600,
+                conn_health_checks=True,
+                ssl_require=True,
+            ),
+        }
+    else:
+        DATABASES = {
+            "default": dj_database_url.config(
+                default=DATABASE_URL,
+                conn_max_age=600,
+                conn_health_checks=True,
+                ssl_require=True,
+            ),
+        }
 else:
     DATABASES = {
         'default': {
